aaea1a60aa2e7110432d2cd242c3c7adf73a178d
Fiqri Syah Redha <wibulo27@gmail.com>
2024-05-31 04:01:30 +0700

    feat: add drop shadow to pokemon card

--- src/pages/home-page/components/pokemon-card.tsx
+++ src/pages/home-page/components/pokemon-card.tsx
@@ -1,4 +1,8 @@
 import { Card, CardBody, CardFooter, Image } from "@nextui-org/react";
+import { useEffect, useState } from "react";
+import useDarkMode from "use-dark-mode";
+
+import { getImageDominantColor } from "@/utils";
 
 export type PokemonCardProps = {
   name: string;
@@ -9,20 +13,48 @@ export const PokemonCard: React.FunctionComponent<PokemonCardProps> = ({
   name,
   imageUrl,
 }) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const darkMode = useDarkMode();
+  const [imageDropShadowColor, setImageDropShadowColor] = useState<
+    [number, number, number]
+  >([0, 0, 0]);
+
+  useEffect(() => {
+    (async () => {
+      if (imageUrl) {
+        const dominantColor = await getImageDominantColor(imageUrl);
+
+        if (dominantColor) {
+          const [r, g, b] = dominantColor.value;
+          setImageDropShadowColor([r, g, b]);
+        }
+      }
+    })();
+  }, [imageUrl]);
+
   return (
     <Card
       shadow="none"
       isPressable
       disableRipple
-      className="group overflow-visible bg-transparent hover:-translate-y-2 transition-all"
+      className="overflow-visible bg-transparent hover:-translate-y-2 transition-all"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
     >
       <CardBody className="overflow-visible p-0">
-        <Image
-          removeWrapper
-          shadow="none"
+        <img
           width="100%"
           alt={`${name} image`}
-          className="drop-shadow-violet dark:drop-shadow-violet-dark w-full h-full object-cover p-1"
+          className="w-full h-full object-cover p-1 transition-all"
+          style={
+            isHovered
+              ? {
+                  filter: `drop-shadow(0 30px 30px rgba(${imageDropShadowColor[0]}, ${imageDropShadowColor[1]}, ${imageDropShadowColor[2]}, ${darkMode.value ? 0.55 : 1}))`,
+                }
+              : {
+                  filter: `drop-shadow(0 30px 30px rgba(${imageDropShadowColor[0]}, ${imageDropShadowColor[1]}, ${imageDropShadowColor[2]}, ${darkMode.value ? 0.4 : 0.8}))`,
+                }
+          }
           src={imageUrl}
         />
       </CardBody>
